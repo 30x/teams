6583dc10564f82510458ff136c8d1fb42933b969
Martin Nally <mnally@apigee.com>
2016-08-09 13:18:02 -0700

    add test directory:

---
+++ test/run-teams-api.sh
@@ -0,0 +1,10 @@
+export IPADDRESS="127.0.0.1"
+export PORT=3002
+export PG_HOST="localhost"
+export PG_USER="martinnally"
+export PG_PASSWORD="martinnally"
+export PG_DATABASE="permissions"
+export COMPONENT="teams"
+export SPEEDUP=10
+
+node teams.js
