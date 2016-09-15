1355cbc7713e51814581d791021b0fddd55b786b
Martin Nally <mnally@apigee.com>
2016-09-15 13:03:44 -0700

    increment docker image version

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,2 @@
 docker build -t teams .
-docker tag -f teams thirtyx/teams:0.0.1
+docker tag -f teams thirtyx/teams:0.0.2
--- docker-push.sh
+++ docker-push.sh
@@ -1,1 +1,1 @@
-docker push thirtyx/teams:0.0.1
+docker push thirtyx/teams:0.0.2
