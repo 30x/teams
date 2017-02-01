4cc01402661648a1f032ce8b5ec0fb8ff698c765
Martin Nally <mnally@google.com>
2017-02-01 11:05:10 -0800

    rename COMPONENT to COMPONENT_NAME

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ADD package.json .
 RUN npm install --production
 ADD . .
 
-ENV COMPONENT="teams" 
+ENV COMPONENT_NAME="teams" 
 ENV PORT=3000
 EXPOSE 3000
 CMD ["node", "teams.js"]
--- test/run-teams-api.sh
+++ test/run-teams-api.sh
@@ -1,6 +1,6 @@
 export IPADDRESS="127.0.0.1"
 export PORT=3002
-export COMPONENT="teams"
+export COMPONENT_NAME="teams"
 export SPEEDUP=10
 export EXTERNAL_SY_ROUTER_HOST="localhost"
 export EXTERNAL_SY_ROUTER_PORT="8080"
